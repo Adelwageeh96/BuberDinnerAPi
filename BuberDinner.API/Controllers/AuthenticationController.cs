67f72da357075973b9a2c416fd961f35bb7c8ff7
Adel Wageeh <adelwageeh222@gmail.com>
2024-03-02 14:10:47 +0200

    Add project files.

---
+++ BuberDinner.API/Controllers/AuthenticationController.cs
@@ -0,0 +1,56 @@
+﻿using BuberDinner.Application.Services.Authentication;
+using BuberDinner.Contracts.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BuberDinner.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly IAuthenticationService _authenticationService;
+
+
+        public AuthenticationController(IAuthenticationService authenticationService)
+        {
+            _authenticationService = authenticationService;
+        }
+
+        [HttpPost("register")]
+        public IActionResult Register(RegisterRequest request)
+        {
+            var authResult = _authenticationService.Register(
+                request.FirstName,
+                request.LastName,
+                request.Email,
+                request.Password
+                );
+            return Ok(new AuthenticationResponse
+            (
+                authResult.User.Id,
+                authResult.User.FirstName,
+                authResult.User.LastName,
+                authResult.User.Email,
+                authResult.Token
+            ));
+        }
+
+        [HttpPost("login")]
+        public IActionResult Login(LoginRequest request)
+        {
+            var authResult = _authenticationService.Login(
+                request.Email,
+                request.Password
+                );
+            return Ok(new AuthenticationResponse
+            (
+                authResult.User.Id,
+                authResult.User.FirstName,
+                authResult.User.LastName,
+                authResult.User.Email,
+                authResult.Token
+            ));
+        }
+    }
+}
---
+++ BuberDinner.API/Program.cs
@@ -0,0 +1,46 @@
+
+using BuberDinner.Application;
+using BuberDinner.Application.Services.Authentication;
+using BuberDinner.Infrastructure;
+
+namespace BuberDinner.API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            {
+                builder.Services
+                    .AddApplication()
+                    .AddInfrastructure(builder.Configuration);
+
+                builder.Services.AddControllers();
+                builder.Services.AddEndpointsApiExplorer();
+                builder.Services.AddSwaggerGen();
+            }
+
+
+
+            var app = builder.Build();
+            {
+                if (app.Environment.IsDevelopment())
+                {
+                    app.UseSwagger();
+                    app.UseSwaggerUI();
+                }
+
+                app.UseHttpsRedirection();
+
+                app.UseAuthorization();
+
+
+                app.MapControllers();
+
+                app.Run();
+            }
+
+
+        }
+    }
+}
